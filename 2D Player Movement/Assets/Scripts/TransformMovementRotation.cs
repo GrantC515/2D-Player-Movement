bde8004a60211c45f3432cd68cbdd0aa666694e0
GrantC515 <111007253+GrantC515@users.noreply.github.com>
2022-09-28 11:02:15 -0700

    Rotation scene finished Rigidbody started

--- 2D Player Movement/Assets/Scripts/TransformMovementRotation.cs
+++ 2D Player Movement/Assets/Scripts/TransformMovementRotation.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class TransformMovementRotation : MonoBehaviour
 {
-     public float speed = 5;
-     
+    public float speed = 5;
+    public float rotationSpeed = 20; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,10 @@ public class TransformMovementRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+        transform.Rotate(Vector3.back, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
